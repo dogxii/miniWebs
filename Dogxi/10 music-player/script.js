b4d8060379fa5c5c2906a470ecfcdc177c7abab3
Dogxi <hi@dogxi.me>
2025-11-09 19:39:20 +0800

    ⚡️

---
+++ Dogxi/10 music-player/script.js
@@ -0,0 +1,11 @@
+/** @type {HTMLButtonElement} */
+const prevBtn = document.querySelector("#prevBtn");
+
+/** @type {HTMLButtonElement} */
+const playBtn = document.querySelector("#playBtn");
+
+/** @type {HTMLButtonElement} */
+const nextBtn = document.querySelector("#nextBtn");
+
+/** @type {HTMLAudioElement} */
+const audioEl = document.querySelector("#musicAudio");
